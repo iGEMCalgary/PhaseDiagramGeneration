4167c11cdb63b3b99eb9cfa9b1abb994678ff5d4
JakeLGH <33042593+JakeLGH@users.noreply.github.com>
2019-07-26 10:29:30 -0600

    Updated the SVM. Fixed an issue with some generated points not being created.

--- SVM/PD_SVM.py
+++ SVM/PD_SVM.py
@@ -1,4 +1,3 @@
-
 #Imports
 from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -12,13 +11,14 @@ import re
 import numpy as np
 import pandas as pd
 
-#For plots
+#For plots and evaluation
 import matplotlib.pyplot as plt
 import statistics
 import time
 import math
 from numpy.polynomial.polynomial import polyfit
 import matplotlib.ticker as plticker
+import plotly.graph_objects as go
 
 #For ML
 from sklearn.svm import SVC
@@ -28,20 +28,6 @@ from sklearn import metrics
 import datetime as dt
 from datetime import timedelta
 from datetime import date
-
-
-
-#Global Settings
-####################################################################################
-#Temperature of the input data
-tempConst = 293.15
-
-#Path for data readable csv file
-dataPath = r"Input\inputPD T{}.csv".format(math.floor(tempConst))
-
-#Save Path for the generated data.
-phaseDiagramSavePath = r"Output\outputPD T{}.csv".format(math.floor(tempConst))
-####################################################################################
 
 #determine the difference between the current time and the given start time
 def timeEndTime(startTime):
@@ -54,30 +40,26 @@ def timeEndTime(startTime):
 	
 	return timeString
 
-#Get an input from the user in the range of [lowerLimit, upperLimit]
-def getUserInput(lowerLimit, upperLimit, prompt):
-	while(True):
-		try:
-			print(prompt)
-			userInput = input()
-			if int(userInput) >= lowerLimit and int(userInput) <= upperLimit:
-				return int(userInput)
-		except ValueError:
-			print("Invalid Input")
-
 #Load the input data and preprocess it
-def loadData():
-
+def loadData(dataPath):
 	#Get the input data and preprocess it	
 	print("Loading phase diagram data...")
 	startTime = time.time()
 		
 	phaseDf = downloadData(dataPath)
-	phaseDf, uniquePhases = formatData(phaseDf)	
+	phaseDf, uniquePhases, tempConst = formatData(phaseDf)	
 		
-	print("Phase diagram data loaded from {}. ".format(dataPath) + timeEndTime(startTime))
+	print("Phase diagram data for T = {}K loaded from {}. ".format(tempConst, dataPath) + timeEndTime(startTime))
 	
-	return [phaseDf, uniquePhases]
+	return [phaseDf, uniquePhases, tempConst]
+
+#Get the file names of every picture in a given folder path
+def filesInFolder(folderPath, specificFiles=[]):	
+	fileList=[]
+	for filename in os.listdir(folderPath):
+		if (len(specificFiles)==0 or filename in specificFiles) and filename[-4:]=='.csv':
+			fileList.append(os.path.join(folderPath, filename))
+	return fileList
 
 #Download input csv data from a given data path and return the dataframe
 def downloadData(dataPath):
@@ -92,26 +74,25 @@ def formatData(phaseDf):
 	phaseDfHeaders = list(phaseDf.columns)
 	phaseCol = phaseDf[phaseDfHeaders[len(phaseDfHeaders)-1]]
 	
-	#Convert last column into consecutive binary column
+	#Get the temperature of the phase diagram
+	tempConst = phaseDf[phaseDfHeaders[3]][0]
 	
+	#Convert last column into consecutive binary column
 	uniquePhases = getUniqueElems(phaseCol)
 	uniquePhases.sort()
 	
-	#phaseCol = [uniquePhases.index(phasePoint) for phasePoint in phaseCol]
-	
+	#Format the phase data
+	#############################################################################################################################################################
 	phaseCol = [phasePoint/15. for phasePoint in phaseCol]
-	
+	#############################################################################################################################################################
+
 	phaseDf[phaseDfHeaders[len(phaseDfHeaders)-1]] = phaseCol
 	
-	return [phaseDf, uniquePhases]
+	return [phaseDf, uniquePhases, tempConst]
 	
 #Get a unique list of each element in the given list
 def getUniqueElems(givenList):
-	uniqueList = []
-	for elem in givenList:
-		if elem not in uniqueList:
-			uniqueList.append(elem)
-	return uniqueList
+	return list(set(givenList))
 	
 #Split the given data into training, validation, and test sets
 #Ensure at least one of every phase is in the train set.
@@ -120,39 +101,38 @@ def splitData(phaseDf, trainTestFrac=0.8):
 	phaseDfHeaders = list(phaseDf.columns)
 	uniquePhases = getUniqueElems(phaseDf[phaseDfHeaders[len(phaseDfHeaders)-1]])
 		
-	#Shuffle and split the input data into training and testing
+	#Shuffle and split the input data into training and testing so that at least
+	#one data point from each phase is in the training data.
 	randState = 0
 	trainValDf = phaseDf.sample(frac=trainTestFrac, random_state=randState)
 	while len(uniquePhases) != len(getUniqueElems(trainValDf[phaseDfHeaders[len(phaseDfHeaders)-1]])):
 		randState = randState + 1
 		trainValDf = phaseDf.sample(frac=trainTestFrac, random_state=randState)
-	
-	testDf = phaseDf.drop(trainValDf.index)
-	
 	print("The number of training data used is {}.".format(len(trainValDf)))
+
+	#Create testing data
+	testDf = phaseDf.drop(trainValDf.index)
 	
 	#Split target header off from phase Df
 	headers = list(phaseDf.columns)
 	targetHeader = headers[len(headers)-1]
-	
 	trainTarget = trainValDf.pop(targetHeader)
 	testTarget = testDf.pop(targetHeader)
-	"""
-	#Scale the dataframe with min max scaling
-	xScaler = MinMaxScaler()
-	trainValDf = pd.DataFrame(xScaler.fit_transform(trainValDf))
-	testDf = pd.DataFrame(xScaler.transform(testDf))
-	"""
+
 	return [trainValDf, trainTarget, testDf, testTarget]
-	
+
+#Define your custom hyperparameters for the model
+def specifyHyperParameters(temperature):
+	#default gamma and C
+	return (0.1,10000.)
 
 #Create and train the SVM
-def getModel(trainX, trainY, uniquePhases):
+def getModel(trainX, trainY, uniquePhases, hyperParams=(0.1,10000.)):
 	startTime = time.time()
-	print("Creating SVM model.")
+	print("\nCreating SVM model.")
 	
-	#gamma and C must be determined through heuristic methods
-	phaseModel = SVC(kernel='rbf', gamma=0.1, C=10000.)
+	#The model is an SVM with an rbf kernel
+	phaseModel = SVC(kernel='rbf', gamma=hyperParams[0], C=hyperParams[1])
 	
 	phaseModel.fit(trainX, trainY)
 	print("SVM Model created. " + timeEndTime(time.time()))
@@ -179,30 +159,9 @@ def evaluatePredictions(model, xData, yData):
 	plotErrDistr(yData, yPredictions)
 	
 	print("Evaluation complete. " + timeEndTime(startTime))
-	
-#Print the most egregious model predictions
-def	printEgregiousPredictions(yData, yPredictions):
-	#These will be false positives with a high confidence
-	egregiousConfidence = 0.8
-	
-	print("\nThe predictions with a confidence of > {}%:".format(egregiousConfidence*100))
-	egregiousPredictionsList = []
-	for testLab, testOut in zip(yData, yPredictions):
-		testPred, testConf = getPrediction(testOut)
-		if testPred != testLab and testConf >= egregiousConfidence:	
-			egregiousPredictionsList.append((testOut, testPred))
-			print("prediction = {},	true value = {}, Confidence = {:5.3f}%".format(
-				   testPred, testLab, testConf*100))
-				   
-	print("Above is Displaying {} of {} ({:5.3f}%) test predictions.".format(
-		  len(egregiousPredictionsList), len(yPredictions), len(egregiousPredictionsList)/len(yPredictions)*100))
-
-	plotNNOutput(egregiousPredictionsList)
-
 
 #Calculate the accuracy of a set
 def calculateError(yData, yPredictions):
-
 	correctCount = 0
 	for testLab, testOut in zip(yData, yPredictions):
 		testPred = getPrediction(testOut)[0]
@@ -259,16 +218,10 @@ def plotErrDistr(yData, yPredictions, ):
 
 
 #Determine the classification and confidence level of a prediction
-def getPrediction(nnOutput):
-	"""
-	prediction = np.argmax(nnOutput)
-	confidence = nnOutput[prediction]
+def getPrediction(modelOutput):
+	return [modelOutput, 1.0]
 	
-	return [prediction, confidence]
-	"""
-	return [nnOutput, 1.0]
-	
-#Plot a subplot for the the NN's classification outputs for a given prediction list
+#Plot a subplot for the the model's classification outputs for a given prediction list
 def plotNNOutput(labeledPredList):
 	displayGridLength = 5
 	displayGridWidth = 6
@@ -283,102 +236,199 @@ def plotNNOutput(labeledPredList):
 		plt.close()
 
 #Use the trained model to densely create the data points for a phase diagram
-def createPhaseDiagram(phaseModel, phaseDfParam):
-	#input dataframe format
-	inputDfHeaders = list(phaseDfParam.columns)
+def createPhaseDiagram(phaseModel, phaseDfParam, tempConst, totalPhases, savePath):
+	#Generate the phase diagram
+	phaseDf = generatePDDF(phaseModel, phaseDfParam, tempConst, 0.01)
+	
+	#Save the final output dataframe to a csv file.
+	print("Saving generated phase diagram.")
+	phaseDiagramSavePath = savePath[:-4] + '_Gen' + savePath[-4:]
+	phaseDf.to_csv(phaseDiagramSavePath, index=False, header=False)
+	print("Phase diagram saved at {}. ".format(phaseDiagramSavePath))
+	
+	#Display the generated phase diagram
+	phaseDf = generatePDDF(phaseModel, phaseDfParam, tempConst, 0.01)
+	phaseDf.pop(list(phaseDf.columns)[-2:-1][0])
+	displayTernaryPD(phaseDf, totalPhases)
+	
+#Generate a phase diagram dataframe 
+def generatePDDF(phaseModel, phaseDfParam, tempConst, denseInterval = 0.01):
+	#Get an empty dataframe of the same form as the input dataframe
+	headers = list(phaseDfParam.columns)
 	phaseDf = phaseDfParam.copy()
 	phaseDf = phaseDf.iloc[-1:-1,]
-	print("THE FOLLOWING DATAFRAME SHOULD BE EMPTY.")
-	print(phaseDf)	
-	
-	#Interval of data points
+
+	#Generate the phase diagram
 	print("Generating phase diagram.")
 	startTime = time.time()
-	denseInterval = 0.01
-	
-	generatedTable = [[] for i in range(len(inputDfHeaders))]
-	#tempConst = 273.15
-	
+	generatedTable = [[] for i in range(len(headers))]
 	i = j = 0
-	while i <= 1.0:		
-		while j <= 1.0 - i:
-			generatedTable[0].append(i)
-			generatedTable[1].append(j)
-			generatedTable[2].append(1.0-i-j)
+	while round(i,5) <= 1.0:		
+		while round(j,5) <= round(1.0 - i, 5):
+			generatedTable[0].append(round(i,5))
+			generatedTable[1].append(round(j,5))
+			generatedTable[2].append(round(1.0-i-j,5))
 			generatedTable[3].append(tempConst)
-			generatedTable[4].append(getPrediction(phaseModel.predict(
-									 pd.DataFrame([[i,j,1.0-i-j,tempConst]]))[0])[0])
-			
+			generatedTable[4].append(round(getPrediction(phaseModel.predict(
+									 pd.DataFrame([[i,j,1.0-i-j,tempConst]]))[0])[0],5))
 			j = j + denseInterval
 		j = 0
 		i = i + denseInterval
 
 	#Put the final output csv together
-	for i in range(len(inputDfHeaders)):
-		phaseDf[inputDfHeaders[i]] = generatedTable[i]
+	for i in range(len(headers)):
+		phaseDf[headers[i]] = generatedTable[i]
 	
-	#Save the final output dataframe to a csv file.
-	print("Saving generated phase diagram.")
-	phaseDf.to_csv(phaseDiagramSavePath, index=False, header=False)
-	print("Phase diagram generated and saved at {}. ".format(phaseDiagramSavePath) + timeEndTime(startTime))
+	print("Phase diagram generated. " + timeEndTime(startTime))
+	return phaseDf
 	
 	
+#Display a phase diagram ternary contour and scatter plot 
+def displayTernaryPD(phaseDf, overallPhases):
+	#Format the data
+	#The total number of phases in existence	
+	totalPhases = overallPhases
+	rawDataScatter = []
+	rawDataContour = [[str(i)] for i in range(totalPhases)]
 	
+	for i in phaseDf.index:
+		a,b,c,p = [phaseDf.iloc[i,j] for j in range(4)]
+		rawDataScatter.append({'Species 1':	a,
+							   'Species 2':	c,
+							   'Species 3':	b,
+							   'Phase':		p})
+		rawDataContour[int(phaseDf.iloc[i,3])].append([phaseDf.iloc[i,0], phaseDf.iloc[i,2], phaseDf.iloc[i,1]])
+		
+	#Display scatter plot
+	displayTernaryPDScatter(rawDataScatter)
 	
+	#Display contour plot 
+	displayTernaryPDContour(rawDataContour)
 	
+#Display the generated phase diagram as a scatterplot
+def displayTernaryPDScatter(rawDataScatter):
+	phaseToColDict = {0:'#8dd3c7', 1:'#ffffb3', 2:'#bebada', 3:'#fb8072', 4:'#80b1d3', 5:'#fdb462'}
+	fig = go.Figure(go.Scatterternary({
+		'mode': 'markers',
+		'a': [i for i in map(lambda x: x['Species 1'], rawDataScatter)],
+		'b': [i for i in map(lambda x: x['Species 2'], rawDataScatter)],
+		'c': [i for i in map(lambda x: x['Species 3'], rawDataScatter)],
+		'text': [i for i in map(lambda x: x['Phase'], rawDataScatter)],
+		'marker': {
+			'symbol': 100,
+			'color':[phaseToColDict[phase] for phase in map(lambda x: x['Phase'], rawDataScatter)],
+			'size': 4,
+			'line': {'width': 2}
+		}
+	}))
 	
+	fig.update_layout({
+		'ternary': {
+			'sum': 100,
+			'aaxis': makeAxis('Species 1', 0),
+			'baxis': makeAxis('<br>Species 2', 45),
+			'caxis': makeAxis('<br>Species 3', -45)
+		},
+		'annotations': [{
+		  'showarrow': False,
+		  'text': 'Simple Ternary Plot with Markers',
+			'x': 0.5,
+			'y': 1.3,
+			'font': { 'size': 15 }
+		}]
+	})
+
+	fig.show()
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+#Make the axes for the ternary phase diagram
+def makeAxis(title, tickangle):
+    return {
+      'title': title,
+      'titlefont': { 'size': 20 },
+      'tickangle': tickangle,
+      'tickfont': { 'size': 15 },
+      'tickcolor': 'rgba(0,0,0,0)',
+      'ticklen': 5,
+      'showline': True,
+      'showgrid': True
+    }	
+
+#Display the generated phase diagram as a countour plot
+def displayTernaryPDContour(rawDataContour):
+	#remove missing phases
+	missingPhases = []
+	for phase in rawDataContour:
+		if len(phase) == 1:
+			missingPhases.append(int(phase[0][0]))
+			
+	colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072',
+			  '#80b1d3','#fdb462','#476264','#787851',
+			  '#489ad3','$e01523','#ef328f','#919191'][:len(rawDataContour)]
+			  
+	for missingPhase in missingPhases:
+		colors.pop(missingPhase)
+		rawDataContour.pop(missingPhase)
+	colors_iterator = iter(colors)
+
+	fig = go.Figure()
+
+	for rawDataPhase in rawDataContour:
+		a = [innerData[0] for innerData in rawDataPhase[1:]]
+		b = [innerData[1] for innerData in rawDataPhase[1:]]
+		c = [innerData[2] for innerData in rawDataPhase[1:]]
+
+		fig.add_trace(go.Scatterternary(
+			text = rawDataPhase[0],
+			a=a, b=b, c=c, mode='lines',
+			line=dict(color='#444', shape='spline'),
+			fill='toself',
+			fillcolor = colors_iterator.__next__()
+		))
+
+	fig.update_layout(title = 'Ternary Contour Plot')
+	fig.show()
 	
 
-#DATA PREPROCESSING
-####################################################################################
 
-#Load and reformat the input data
-phaseDf, uniquePhases = loadData()
+#Do for each file in the folder
+inputFolderPath = r"Experimental Data P1"
+outputFolderPath = r"Interpolated Data P1" 
+for inputFilePath in filesInFolder(inputFolderPath, specificFiles=[]):
+	print("\n############################################################################################")
+	#DATA PREPROCESSING
+	####################################################################################
+	#Load and reformat the input data
+	phaseDf, uniquePhases, tempConst = loadData(inputFilePath)
 
-#Split the given data into training, validation, and test data
-trainDataX, trainDataY, testDataX, testDataY = splitData(phaseDf, trainTestFrac=0.05)
-
-####################################################################################
-
-
-
-#CREATE AND EVALUATE THE SVM
-####################################################################################
-
-#Create the model
-phaseModel = getModel(trainDataX, trainDataY, uniquePhases)
-
-#Calculate the test error of the model
-evaluateModel(phaseModel, testDataX, testDataY)
-
-#Make predictions for the training data
-print("\n~~~~~~~~~~~~~~~~~~~~~~~~~Evaluating Training Data Performance~~~~~~~~~~~~~~~~~~~~~~~~~")
-evaluatePredictions(phaseModel, trainDataX, trainDataY)
-
-#Make predictions for the testing data
-print("\n~~~~~~~~~~~~~~~~~~~~~~~~~Evaluating Testing Data Performance~~~~~~~~~~~~~~~~~~~~~~~~~")
-evaluatePredictions(phaseModel, testDataX, testDataY)
-
-####################################################################################
+	#Split the given data into training, validation, and test data
+	trainDataX, trainDataY, testDataX, testDataY = splitData(phaseDf, trainTestFrac=0.80)
+	####################################################################################
 
 
-#PRODUCE FINAL PHASE DIAGRAM POINTS
-####################################################################################
-print("\n~~~~~~~~~~~~~~~~~~~~~~~~~Fully Interpolating Phase Diagram~~~~~~~~~~~~~~~~~~~~~~~~~")
-createPhaseDiagram(phaseModel, phaseDf)
+	#CREATE AND EVALUATE THE SVM
+	####################################################################################
+	#Create the model
+	phaseModel = getModel(trainDataX, trainDataY, uniquePhases, specifyHyperParameters(tempConst))
 
-####################################################################################
+	#Calculate the test error of the model
+	evaluateModel(phaseModel, testDataX, testDataY)
+
+	#Make predictions for the training data
+	print("\n~~~~~~~~~~~~~~~~~~~~~~~~~Evaluating Training Data Performance~~~~~~~~~~~~~~~~~~~~~~~~~")
+	evaluatePredictions(phaseModel, trainDataX, trainDataY)
+
+	#Make predictions for the testing data
+	print("\n~~~~~~~~~~~~~~~~~~~~~~~~~Evaluating Testing Data Performance~~~~~~~~~~~~~~~~~~~~~~~~~")
+	evaluatePredictions(phaseModel, testDataX, testDataY)
+	####################################################################################
 
 
+	#PRODUCE FINAL PHASE DIAGRAM POINTS
+	####################################################################################
+	print("\n~~~~~~~~~~~~~~~~~~~~~~~~~Fully Interpolating Phase Diagram~~~~~~~~~~~~~~~~~~~~~~~~~")
+	#Specify the total phases of the entire system
+	totalPhases = 10
+	outputFilePath = os.path.join(outputFolderPath, inputFilePath[len(inputFolderPath)+1:])
+	createPhaseDiagram(phaseModel, phaseDf, tempConst, totalPhases, outputFilePath)
+	####################################################################################
+	print("\n############################################################################################\n")
